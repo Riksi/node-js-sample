6dfe3cd9217b2eb4eb833732b0f66a17629346b0
User <user@Users-MacBook-Pro-2.local>
2017-01-10 12:58:47 +0000

    Img size

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("<img style='width:25%;height:25%;' src ='https://upload.wikimedia.org/wikipedia/commons/8/80/Metal_dragon_half_frontal_view.jpg'/>");
+  response.send("<img style='width:35%;height:35%;' src ='https://upload.wikimedia.org/wikipedia/commons/8/80/Metal_dragon_half_frontal_view.jpg'/>");
 });
 
 var port = process.env.PORT || 5000;
